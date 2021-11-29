861b6f0119606c113f4281f1a305ded7fa3a1f5f
Grace Li <graceli.contact@gmail.com>
2021-11-29 14:40:50 -0500

    first commit

---
+++ popup-init.js
@@ -0,0 +1,11 @@
+// Check if the user is signed in
+
+function init() {
+    chrome.runtime.sendMessage({ message: 'is_user_signed_in' }, function (response) {
+        if (response.message === 'success' && response.payload) {
+            window.location.replace('./main.html');
+        }
+    });
+}
+
+init();
